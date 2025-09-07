3c59b76a9f669cf42de2f46e9a046b3221b5a5d3
Роман Зорин <sgena170315@gmail.com>
2025-09-07 15:26:03 +0300

    Задание 2: Класс «Банковский счёт»

---
+++ 2_Task/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿namespace _2_Task
+{
+    internal class BankAccount
+    {
+        private static string? AccountNumber = "2200 2113 3450 3204";
+        private decimal Balance = 0m;
+
+        public void Deposit(decimal amount)
+        {
+            if(amount > 0)
+                this.Balance += amount;
+        }
+        public void Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <= this.Balance)
+                this.Balance -= amount;
+            else
+                Console.WriteLine("Недостаточно средств!");
+        }
+        public void PrintBalance()
+        {
+            Console.WriteLine($"Текущий баланс: {Balance}");
+        }
+    }
+}
---
+++ 2_Task/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _2_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount BA = new BankAccount();
+            BA.Deposit(1000);
+            BA.Withdraw(500);
+            BA.PrintBalance();
+        }
+    }
+}
