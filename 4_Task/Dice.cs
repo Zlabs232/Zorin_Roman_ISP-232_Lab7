b7f76e86fe7040b73e4fb89c5749739c9b762904
Роман Зорин <sgena170315@gmail.com>
2025-09-07 15:39:53 +0300

    Задание 4: Класс «Игральный кубик»

---
+++ 4_Task/Dice.cs
@@ -0,0 +1,19 @@
+﻿namespace _4_Task
+{
+    internal class Dice
+    {
+        Random random = new Random();
+        public int _sides;
+
+        public Dice(int _sides)
+        {
+            this._sides = _sides;
+        }
+
+        public void Roll()
+        {
+            Console.WriteLine($"{_sides}-гранный кубик: {random.Next(1, _sides + 1)},{random.Next(1, _sides + 1)},{random.Next(1, _sides + 1)}");
+        }
+
+    }
+}
---
+++ 4_Task/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _4_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice kub = new(6);
+            Dice kub2 = new(12);
+            kub2.Roll();
+            kub.Roll();
+
+
+
+        }
+    }
+}
