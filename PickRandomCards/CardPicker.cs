35b06437271985b8bbd89ee28ad402d6198ba1ce
Роман Зорин <sgena170315@gmail.com>
2025-09-07 15:04:38 +0300

    было создано консольное приложение PickRandomCards

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace PickRandomCards
+{
+    internal class CardPicker
+    {
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Туз";
+            if (value == 11) return "Валет";
+            if (value == 12) return "Дама";
+            if (value == 13) return "Король";
+            return value.ToString();
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1,5);
+            if (value == 1) return "Пик";
+            if (value == 2) return "Червей";
+            if (value == 3) return "Треф";
+            return "Бубен";
+        }
+    }
+}
---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во карт для выбора: ");
+            string? line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine($"Карта: {card}");
+                }
+            }
+            else
+                Console.WriteLine("Введите корректное число.");
+        }
+    }
+}
