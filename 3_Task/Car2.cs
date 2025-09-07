86898259ad08518ac9d6391959b9f0cb7ec5bd56
Роман Зорин <sgena170315@gmail.com>
2025-09-07 15:31:36 +0300

    Задание 3: Класс «Автомобиль»

---
+++ 3_Task/Car2.cs
@@ -0,0 +1,31 @@
+﻿class Car2
+{
+    public string Model;
+    public int Speed;
+    public int MaxSpeed;
+
+    public Car2(string model, int maxSpeed)
+    {
+        this.Model = model;
+        this.MaxSpeed = maxSpeed;
+        this.Speed = 0;
+    }
+    public void Accelerate(int delta)
+    {
+        if (delta > 0)
+        {
+            this.Speed += delta;
+            if (this.Speed > this.MaxSpeed)
+                this.Speed = this.MaxSpeed;
+        }
+    }
+    public void Brake()
+    {
+        this.Speed = 0;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Модель: {this.Model}, Текущая скорость: {this.Speed} км/ч");
+    }
+}
---
+++ 3_Task/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _3_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car2 myCar = new Car2("Tesla Model S", 250);
+
+            myCar.Accelerate(120);
+            myCar.PrintInfo(); 
+
+            myCar.Brake();
+            Console.WriteLine($"После торможения: {myCar.Speed} км/ч");
+        }
+    }
+}
