9a7638802ccca4daf4191a37866c4e5d65340be3
Роман Зорин <sgena170315@gmail.com>
2025-09-07 15:15:46 +0300

    Задание 1: Класс «Книга»

---
+++ 1_Task/Book.cs
@@ -0,0 +1,32 @@
+﻿namespace _1_Task
+{
+    internal class Book
+    {
+        public string? book;
+        public string? author;
+        public int year;
+        public bool isAvailable;
+
+        public Book()
+        {
+            book = "неизвестно";
+            author = "неизвестно";
+            year = 19;
+            isAvailable = false;
+        }
+
+        public Book(string? book, string? author, int year, bool isAvailable)
+        {
+            this.book = book;
+            this.author = author;
+            this.year = year;
+            this.isAvailable = isAvailable;
+        }
+
+        public void printInfo()
+        {
+          
+            Console.WriteLine($"Книга: {book}, Автор: {author} , Год: {year} , Доступна для выдачи: {(isAvailable == true ? "Да" : "Нет")} ");
+        }
+    }
+}
---
+++ 1_Task/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _1_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book fBook = new Book();
+            fBook.printInfo();
+
+            Book sBook = new("Война и мир", "Толстой", 1869, true);
+            sBook.printInfo();
+            
+
+        }
+    }
+}
