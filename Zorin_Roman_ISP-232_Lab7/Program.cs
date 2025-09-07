d3461523a10772743d9e78d28e45ecca1aa39d1e
Роман Зорин <sgena170315@gmail.com>
2025-09-07 13:59:20 +0300

    Add project files.

---
+++ Zorin_Roman_ISP-232_Lab7/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Zorin_Roman_ISP_232_Lab7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Student Denis = new Student();
+            Console.WriteLine(Denis);
+        }
+    }
+
+    class Student
+    {
+        string? name;
+        int age;
+        string? group;
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {name}, Возраст: {age}, Группа: {group}.");
+        }
+    }
+}
